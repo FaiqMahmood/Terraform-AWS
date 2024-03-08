aa07768ab3c2f9184f058350ddb7efeebde730bf
FaiqMahmood <78402839+FaiqMahmood@users.noreply.github.com>
2024-03-08 12:13:49 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,8 +5,8 @@ provider "aws" {
 resource "aws_instance" "terraform-ec2" {
   ami           = "ami-07d9b9ddc6cd8dd30" # Specify an appropriate AMI ID
   instance_type = "t2.micro"
-  subnet_id     = "subnet-01cf40b9bac241b2d"
-  key_name      = "jenkins"
+  subnet_id     = "subnet-01cf40b9acBc2767b2d" # Specify an appropriate subnet_id 
+  key_name      = "aws-key-pair" # Specify an AWS Key pair
 
   tags = {
     Name = "terraform-ec2"
