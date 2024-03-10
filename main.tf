2764f1d5d24e3a11d315e7d603c6e7b90353647e
Faiq Mahmood <faiqmahmood95@gmail.com>
2024-03-10 10:11:44 +0100

    fixed modules in main.tf

--- main.tf
+++ main.tf
@@ -2,10 +2,35 @@ provider "aws" {
   region = "us-east-1"
 }
 
+variable "ami_value" {
+  description = "value for the ami"
+}
+
+variable "instance_type_value" {
+  description = "value for the type of instance"
+}
+
+variable "subnet_id_value" {
+  description = "value for the subnet id"
+}
+
+variable "key_name" {
+  description = "Value AWS key-value pair"
+}
+
+variable "bucket_name" {
+  description = "The name of the bucket"
+}
+
 module "create-ec2-instance" {
-  source = "modules/ec2-instance/"
+  source              = "./modules/ec2-instance/"
+  ami_value           = var.ami_value
+  instance_type_value = var.instance_type_value
+  subnet_id_value     = var.subnet_id_value
+  key_name            = var.key_name
 }
 
 module "create-s3-bucket" {
-  source = "modules/s3-bucket/"
+  source      = "./modules/s3-bucket/"
+  bucket_name = var.bucket_name
 }
